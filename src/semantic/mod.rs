539e41600108561d0d09f0169bf641c55283994c
Alexander Kiselev <github@akiselev.com>
2026-08-16 11:32:30 -0700

    add semantic accessibility architecture

---
+++ src/semantic/mod.rs
@@ -0,0 +1,13 @@
+pub mod action;
+pub mod backend;
+pub mod graph;
+pub mod model;
+pub mod observation;
+pub mod selector;
+
+pub use action::*;
+pub use backend::*;
+pub use graph::*;
+pub use model::*;
+pub use observation::*;
+pub use selector::*;
