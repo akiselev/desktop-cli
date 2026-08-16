f53bbea486e7679c01e11b890b74c40e28635f93
Alexander Kiselev <github@akiselev.com>
2026-08-16 12:07:39 -0700

    compile direct AT-SPI provider in Linux matrix

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -5,8 +5,12 @@ pub mod mock;
 pub mod windows;
 #[cfg(windows)]
 pub mod windows_uia;
+#[cfg(target_os = "linux")]
+pub mod linux_atspi;
 
 pub use legacy::*;
 pub use mock::*;
 #[cfg(windows)]
 pub use windows_uia::*;
+#[cfg(target_os = "linux")]
+pub use linux_atspi::*;
